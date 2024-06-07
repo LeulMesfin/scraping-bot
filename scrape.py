68111a27b811c1b297e23433a7234bb94d508fe0
Leul Mesfin <leulmesfin152025@gmail.com>
2024-06-07 11:50:48 -0400

    printed out job title, email, and app link

--- scrape.py
+++ scrape.py
@@ -11,15 +11,31 @@ page_list = ["hbk-strats-developer.html", "open-data-services-co-operative-pytho
 # print out title from first page, email, appliction link
 for i in range(0, len(page_list), 1):
     main_url = url + page_list[i]
-    print(main_url)
+    #print(main_url)
     resp = requests.get(main_url)
-
     soup = BeautifulSoup(resp.content, "html.parser")
-    job_list = soup.find_all("div", class_="job")
 
-    print(job_list)
+    job_title = soup.find("article").find("h1")
+    print("Job Title:", job_title.text.strip(), end="\n")
 
-    # # print out all jobs, add 2 new lines @ end of job string
-    for job in job_list:
-        job_title = job.find("h1").find("a") # href="/jobs/hbk-strats-developer.html") #href="/jobs/hbk-strats-developer.html")
-        print(job_title.text, end="\n"*2)
+    contact_elts = soup.find("article").find(class_="contact").find_all("div")#.find(string=" Email ")
+    #print(contact_elts)
+    #print(email.text, end="\n"*2)
+
+
+    # job_list = soup.find_all("div", class_="job")
+
+    # print(job_list)
+
+    # # # print out all jobs, add 2 new lines @ end of job string
+
+    for job in contact_elts:
+        
+        #print(job, "\n"*2)
+        #print("-------")
+        email = job.find("a") # href="/jobs/hbk-strats-developer.html") #href="/jobs/hbk-strats-developer.html")
+        #print(email)
+        if email != None:
+            print(email.text.strip())
+
+    print("\n"*2)
